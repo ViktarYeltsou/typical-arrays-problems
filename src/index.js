21737b6ee4c69ec67d6b2d59b8d870cf4d786583
ViktarYeltsou <78786714+ViktarYeltsou@users.noreply.github.com>
2021-07-15 11:20:59 +0300

    fix: min function correction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min (array) {
-  return !array || !array.length ? 0 : array.sort()[0];
+  return !array || !array.length ? 0 : array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
